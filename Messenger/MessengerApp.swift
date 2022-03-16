7037584702795df1244db557535f0e4a5af0de74
florianzl <f.zitlau@web.de>
2022-03-16 21:37:33 +0100

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Florian Zitlau on 16.03.22.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
